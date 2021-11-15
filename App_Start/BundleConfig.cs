3816281f8d905b88cd6abdcf70ffd95964885098
bbautista73 <bbautista73@gmail.com>
2021-11-14 19:54:31 -0600

    updating home index

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -21,12 +21,20 @@ namespace NexusMD.MVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/adminlte/plugins/fontawesome-free/css/all.min.css",
                       "~/adminlte/css/adminlte.min.css",
-                      "~/Content/site.css"));
+                      "~/adminlte/plugins/fullcalendar/main.min.css",
+                      "~/adminlte/plugins/fullcalendar-daygrid/main.min.css",
+                      "~/adminlte/plugins/fullcalendar-bootstrap/main.min.css",
+                      "~/adminlte/plugins/fontawesome-free/css/all.min.css",
+                      "~/Content/site.css"
+                      ));
 
             bundles.Add(new ScriptBundle("~/adminlte/js").Include(
-                       "~/adminlte/js/adminlte.min.js"));
+                       "~/adminlte/js/adminlte.min.js",
+                       "~/adminlte/plugins/fullcalendar/main.js",
+                       "~/adminlte/plugins/fullcalendar-daygrid/main.js",
+                       "~/adminlte/plugins/fullcalendar-bootstrap/main.js"
+                       ));
         }
     }
 }
